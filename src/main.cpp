39b753b6632dc9f15d0a85b27df7b10c595429ee
tomorrow9913 <tomorrow9913@gmail.com>
2020-11-13 01:09:38 +0900

    fix : 빌드가 안되던 오류 수정
    See also: #1

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 /****************************************************************
- * File Name:  main.c
+ * File Name:  main.cpp
  *
  * Description: 은행 시뮬레이션 메인 프로그램
  *
@@ -18,124 +18,13 @@
  * NAME_LEN		const int	이름의 길이를 설정함
  *				enum
  ****************************************************************/
+#include <iostream>
 #include "Account.h"
+#include "AccountHandler.h"
 
 using namespace std;
 
-
-
 enum { MAKE = 1, DEPOSIT, WITHDRAW, INQUIRE, EXIT };
-
-
-/*
- * 클래스 이름: AccountHandler
- * 클래스 유형: 컨트롤(Control) 클래스
- */
-
-class AccountHandler
-{
-private:
-	Account* accArr[100];
-	int accNum;
-
-public:
-	AccountHandler();
-	void ShowMenu(void) const;
-	void MakeAccount(void);
-	void DepositMoney(void);
-	void WithdrawMoney(void);
-	void ShowAllAccInfo(void) const;
-	~AccountHandler();
-};
-
-void AccountHandler::ShowMenu(void) const
-{
-	cout << "-----Menu------" << endl;
-	cout << "1. 계좌개설" << endl;
-	cout << "2. 입    금" << endl;
-	cout << "3. 출    금" << endl;
-	cout << "4. 계좌정보 전체 출력" << endl;
-	cout << "5. 프로그램 종료" << endl;
-}
-
-void AccountHandler::MakeAccount(void)
-{
-	int id;
-	char name[NAME_LEN];
-	int balance;
-
-	cout << "[계좌개설]" << endl;
-	cout << "계좌ID: ";	cin >> id;
-	cout << "이  름: ";	cin >> name;
-	cout << "입금액: ";	cin >> balance;
-	cout << endl;
-
-	accArr[accNum++] = new Account(id, balance, name);
-}
-
-void AccountHandler::DepositMoney(void)
-{
-	int money;
-	int id;
-	cout << "[입    금]" << endl;
-	cout << "계좌ID: ";	cin >> id;
-	cout << "입금액: ";	cin >> money;
-
-	for (int i = 0; i < accNum; i++)
-	{
-		if (accArr[i]->GetAccID() == id)
-		{
-			accArr[i]->Deposit(money);
-			cout << "입금완료" << endl << endl;
-			return;
-		}
-	}
-	cout << "유효하지 않은 ID 입니다." << endl << endl;
-}
-
-void AccountHandler::WithdrawMoney(void)
-{
-	int money;
-	int id;
-	cout << "[출    금]" << endl;
-	cout << "계좌ID: ";	cin >> id;
-	cout << "출금액: ";	cin >> money;
-
-	for (int i = 0; i < accNum; i++)
-	{
-		if (accArr[i]->GetAccID() == id)
-		{
-			if (accArr[i]->Withdraw(money) == 0)
-			{
-				cout << "잔액부족" << endl << endl;
-				return;
-			}
-
-			cout << "출금완료" << endl << endl;
-			return;
-		}
-	}
-	cout << "유효하지 않은 ID 입니다." << endl << endl;
-}
-
-AccountHandler::AccountHandler() : accNum(0)
-{  }
-
-void AccountHandler::ShowAllAccInfo(void) const
-{
-	for (int i = 0; i < accNum; i++)
-	{
-		accArr[i]->ShowAccInfo();
-		cout << endl;
-	}
-}
-
-AccountHandler::~AccountHandler()
-{
-	for (int i = 0; i < accNum; i++)
-		delete accArr[i];
-}
-
 
 /*
  * 컨트롤 클래스 AccountHandler 중심으로 변경된 main 함수
